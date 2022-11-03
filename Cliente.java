7d964424ed0b3359c28220cf68d9363adf97d199
Yuri Barbosa Pires <59899845+coldblu@users.noreply.github.com>
2022-11-03 00:04:10 -0300

    GUI update
    Adicionado area para exibir usuario online

--- Cliente.java
+++ Cliente.java
@@ -7,20 +7,25 @@ import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
+import java.util.ArrayList;
 
 public class Cliente extends JFrame implements ActionListener{
-
+    private static ArrayList<String> names = new ArrayList();
+    String[] nomes = {"0","sd33333a"};
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private String username;
     //Variáveis GUI
-    private JPanel painel = new JPanel(), msg = new JPanel();
+    private JPanel painel = new JPanel(), msg = new JPanel(),users = new JPanel(), geral = new JPanel();
     private JLabel lblHistorico = new JLabel("Historico de Mensagens"), lbMensagem = new JLabel("Mensagem");
+    private JLabel lbOnline = new JLabel("Pessoas Online");
     private JButton btSend = new JButton("Enviar");
     private JTextField tfMensagem  = new JTextField(20);
     private JTextArea taHistorico = new JTextArea(10, 20);
-    private JScrollPane scroll = new JScrollPane(this.taHistorico);
+    private JScrollPane scroll = new JScrollPane(this.taHistorico);    
+    private JList list = new JList(nomes);
+    private JScrollPane scrollList = new JScrollPane(this.list);
 
     //Construtor
     public Cliente(Socket socket, String username){
@@ -40,27 +45,37 @@ public class Cliente extends JFrame implements ActionListener{
     public void tela(){
         
         //Tela
+            //Painel Configs
             this.painel.setLayout( new BoxLayout(painel, BoxLayout.Y_AXIS));
 	    	this.painel.setBackground(Color.LIGHT_GRAY);
 
             this.taHistorico.setEditable(false);
             this.taHistorico.setBackground(new Color(240, 240, 240));
-            
-            this.btSend.addActionListener(this);
-            
+
+            //Botão + caixa de texto
+            this.btSend.addActionListener(this);            
             this.msg.add(this.tfMensagem);
             this.msg.add(this.btSend);
-
             
             this.painel.add(lblHistorico);
             this.painel.add(scroll);
             this.painel.add(lbMensagem);
             this.painel.add(msg);
-
-            this.tfMensagem.setText("");
             
+            this.tfMensagem.setText("");
+            //Lista de usuarios onlines            
+            this.users.setLayout( new BoxLayout(users, BoxLayout.Y_AXIS));
+            this.users.setBackground(Color.LIGHT_GRAY);
+            users.add(lbOnline);
+            users.add(scrollList);
+            //Lego
+            this.geral.setLayout( new BorderLayout());
+            geral.add(painel,BorderLayout.CENTER);
+            geral.add(users,BorderLayout.EAST);
+
+            //Window configs
             this.setTitle("Dolphins Chat");
-            this.setContentPane(painel);
+            this.setContentPane(geral);
             this.setLocationRelativeTo(null);
             this.setDefaultCloseOperation(EXIT_ON_CLOSE);
             this.setResizable(false);
@@ -93,6 +108,7 @@ public class Cliente extends JFrame implements ActionListener{
                     bufferedWriter.flush();
                 } catch (Exception e) {
                     // TODO: handle exception
+                    e.printStackTrace();
                 }
                 
                 while(socket.isConnected()){

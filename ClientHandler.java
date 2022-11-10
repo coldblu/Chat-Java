fe0379e0fe5a9e5d506113e9c1f8f872cd31269c
Yuri Barbosa Pires <59899845+coldblu@users.noreply.github.com>
2022-11-10 15:08:19 -0300

    UI update
    list of users connected to the chat implemented

--- ClientHandler.java
+++ ClientHandler.java
@@ -5,73 +5,119 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
     public static ArrayList<ClientHandler> clientHandlers = new ArrayList();
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private String clientUsername;
 
-    //Construtor
-    public ClientHandler(Socket socket){
+    // Construtor
+    public ClientHandler(Socket socket) {
         try {
             this.socket = socket;
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.clientUsername = bufferedReader.readLine();
             clientHandlers.add(this);
-            broadcastMessage("SERVER: "+ clientUsername + " entrou no chat!");
+            listUsers();
+            broadcastMessage("All;SERVER: " + clientUsername + " entrou no chat!");
         } catch (Exception e) {
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
 
-
-
     @Override
-    public void run(){
+    public void run() {
         String messageFromClient;
 
-            while(socket.isConnected()){
-                try {
-                    messageFromClient = bufferedReader.readLine();
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                String[] check = messageFromClient.split(";",2);
+                if(check[0].equals("All") || check[0].equals(clientUsername)){
                     broadcastMessage(messageFromClient);
-                } catch (Exception e) {
-                    closeEverything(socket, bufferedReader, bufferedWriter);
-                    break;
                 }
+                else{
+                    privateMessage(messageFromClient);
+                }
+                
+                listUsers();
+            } catch (Exception e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
             }
+        }
     }
 
-    public void broadcastMessage(String messageToSend){
-        for(ClientHandler clientHandler : clientHandlers){
+    public void broadcastMessage(String messageToSend) {
+        for (ClientHandler clientHandler : clientHandlers) {
             try {
-                if(!clientHandler.clientUsername.equals(clientUsername)){
-                    clientHandler.bufferedWriter.write(messageToSend);
+                String[] splitIt = messageToSend.split(";", 2);
+                if (splitIt[0].equals("All") || splitIt[0].equals(clientUsername)) {
+                    if (!clientHandler.clientUsername.equals(clientUsername)) {
+                        clientHandler.bufferedWriter.write("All;" + splitIt[1]);
+                        clientHandler.bufferedWriter.newLine();
+                        clientHandler.bufferedWriter.flush();
+                    }
+                } else if (splitIt[0].equals("3")) {
+                    clientHandler.bufferedWriter.write("3;" + splitIt[1]);
                     clientHandler.bufferedWriter.newLine();
                     clientHandler.bufferedWriter.flush();
                 }
+
             } catch (Exception e) {
                 closeEverything(socket, bufferedReader, bufferedWriter);
             }
         }
     }
 
-    public void removeClientHandler(){
-        clientHandlers.remove(this);
-        broadcastMessage("SERVER: " + clientUsername + " saiu do chat!\n");
+    public void privateMessage(String message){
+        try {
+            String[] splitIt = message.split(";",2);
+            for (ClientHandler clientHandler : clientHandlers) {
+                if (clientHandler.clientUsername.equals(splitIt[0])) {
+                    clientHandler.bufferedWriter.write("1;"+ splitIt[1]);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        
     }
 
-    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+    public void listUsers() {        
+            try {
+                StringBuilder users = new StringBuilder();
+                for (ClientHandler clientHandler : clientHandlers) {
+                    users.append(clientHandler.clientUsername);
+                    users.append(";");
+                }                
+                broadcastMessage("3;" + users);
+
+            } catch (Exception e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+    }
+
+    public void removeClientHandler() {
+        clientHandlers.remove(this);
+        listUsers();
+        broadcastMessage("All;SERVER: " + clientUsername + " saiu do chat!\n");
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
         removeClientHandler();
         try {
-            if(bufferedReader != null){
+            if (bufferedReader != null) {
                 bufferedReader.close();
             }
-            if(bufferedWriter != null){
+            if (bufferedWriter != null) {
                 bufferedWriter.close();
             }
-            if(socket != null){
+            if (socket != null) {
                 socket.close();
             }
         } catch (Exception e) {
--- Cliente.java
+++ Cliente.java
@@ -4,37 +4,38 @@ import java.util.Scanner;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JTextArea;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.util.ArrayList;
 
-public class Cliente extends JFrame implements ActionListener{
-    private static ArrayList<String> names = new ArrayList();
-    String[] nomes = {"0","sd33333a"};
+public class Cliente extends JFrame implements ActionListener,ListSelectionListener {
+    
+    public String whosWho = "All";
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private String username;
-    //Variáveis GUI
-    private JPanel painel = new JPanel(), msg = new JPanel(),users = new JPanel(), geral = new JPanel();
+    // Variáveis GUI
+    private JPanel painel = new JPanel(), msg = new JPanel(), users = new JPanel(), geral = new JPanel();
     private JLabel lblHistorico = new JLabel("Historico de Mensagens"), lbMensagem = new JLabel("Mensagem");
     private JLabel lbOnline = new JLabel("Pessoas Online");
     private JButton btSend = new JButton("Enviar");
-    private JTextField tfMensagem  = new JTextField(20);
+    private JTextField tfMensagem = new JTextField(20);
     private JTextArea taHistorico = new JTextArea(10, 20);
-    private JScrollPane scroll = new JScrollPane(this.taHistorico);    
-    private JList list = new JList(nomes);
+    private JScrollPane scroll = new JScrollPane(this.taHistorico);
+    private DefaultListModel modelList = new DefaultListModel<String>();
+    private JList list = new JList();
     private JScrollPane scrollList = new JScrollPane(this.list);
 
-    //Construtor
-    public Cliente(Socket socket, String username){
+    // Construtor
+    public Cliente(Socket socket, String username) {
         try {
             this.socket = socket;
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            this.username = username; 
-            
+            this.username = username;
+
             tela();
 
         } catch (Exception e) {
@@ -42,65 +43,71 @@ public class Cliente extends JFrame implements ActionListener{
         }
     }
 
-    public void tela(){
+    public void tela() {
+
+        // Tela
+        // Painel Configs
+        this.painel.setLayout(new BoxLayout(painel, BoxLayout.Y_AXIS));
+        this.painel.setBackground(Color.LIGHT_GRAY);
+
+        this.taHistorico.setEditable(false);
+        this.taHistorico.setBackground(new Color(240, 240, 240));
+
+        // Botão + caixa de texto
+        this.btSend.addActionListener(this);
+        this.msg.add(this.tfMensagem);
+        this.msg.add(this.btSend);
+
+        this.painel.add(lblHistorico);
+        this.painel.add(scroll);
+        this.painel.add(lbMensagem);
+        this.painel.add(msg);
+
+        this.tfMensagem.setText("");
+        // List
         
-        //Tela
-            //Painel Configs
-            this.painel.setLayout( new BoxLayout(painel, BoxLayout.Y_AXIS));
-	    	this.painel.setBackground(Color.LIGHT_GRAY);
+        this.list.setModel(modelList);
+        //this.list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        this.list.addListSelectionListener(this);
 
-            this.taHistorico.setEditable(false);
-            this.taHistorico.setBackground(new Color(240, 240, 240));
+        this.users.setLayout(new BoxLayout(users, BoxLayout.Y_AXIS));
+        this.users.setBackground(Color.LIGHT_GRAY);
+        users.add(lbOnline);
+        users.add(scrollList);
+        // Lego
+        this.geral.setLayout(new BorderLayout());
+        geral.add(painel, BorderLayout.CENTER);
+        geral.add(users, BorderLayout.EAST);
 
-            //Botão + caixa de texto
-            this.btSend.addActionListener(this);            
-            this.msg.add(this.tfMensagem);
-            this.msg.add(this.btSend);
-            
-            this.painel.add(lblHistorico);
-            this.painel.add(scroll);
-            this.painel.add(lbMensagem);
-            this.painel.add(msg);
-            
-            this.tfMensagem.setText("");
-            //Lista de usuarios onlines            
-            this.users.setLayout( new BoxLayout(users, BoxLayout.Y_AXIS));
-            this.users.setBackground(Color.LIGHT_GRAY);
-            users.add(lbOnline);
-            users.add(scrollList);
-            //Lego
-            this.geral.setLayout( new BorderLayout());
-            geral.add(painel,BorderLayout.CENTER);
-            geral.add(users,BorderLayout.EAST);
+        // Window configs
+        this.setTitle("Dolphins Chat");
+        this.setContentPane(geral);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+        // this.pack();
+        this.setSize(600, 400);
+        this.setVisible(true);
 
-            //Window configs
-            this.setTitle("Dolphins Chat");
-            this.setContentPane(geral);
-            this.setLocationRelativeTo(null);
-            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-            this.setResizable(false);
-            //this.pack();
-            this.setSize(600, 400);
-            this.setVisible(true);
-            
-			
     }
-    //Mandar mensagens
-    public void sendMessage(String mesage){
+
+    // Mandar mensagens
+    public void sendMessage(String mesage) {
         try {
-                bufferedWriter.write(username + " diz -> " + mesage);
-                taHistorico.append(username + " diz -> " + tfMensagem.getText() + "\r\n");                
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
+            bufferedWriter.write(whosWho + ";" + username + " diz -> " + mesage);
+            taHistorico.append(username + " diz -> " + tfMensagem.getText() + "\r\n");
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+            System.out.println(whosWho);
         } catch (Exception e) {
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
 
-    public void listenForMessage(){
-        new Thread(new Runnable(){
+    public void listenForMessage() {
+        new Thread(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
                 String msgFromGroupChat;
                 try {
                     bufferedWriter.write(username);
@@ -110,12 +117,22 @@ public class Cliente extends JFrame implements ActionListener{
                     // TODO: handle exception
                     e.printStackTrace();
                 }
-                
-                while(socket.isConnected()){
+
+                while (socket.isConnected()) {
                     try {
                         msgFromGroupChat = bufferedReader.readLine();
-                        taHistorico.append(msgFromGroupChat+"\n");
-                        //System.out.println(msgFromGroupChat);
+                        String[] splitThis = msgFromGroupChat.split(";", 2);
+                        if (splitThis[0].equals("All")) {
+                            taHistorico.append(splitThis[1] + "\n");
+                        } else if (splitThis[0].equals("3")) {
+                            refreshList(splitThis[0], splitThis[1]);
+                            // taHistorico.append(splitThis[1] + "\n");
+                        }else if(splitThis[0].equals("1")){
+                            //String[] splitAgain = splitThis[1].split(";",2);
+                            taHistorico.append("[Privado]" + splitThis[1] + "\n");
+                        }
+                        // taHistorico.append(msgFromGroupChat + "\n");
+                        // System.out.println(msgFromGroupChat);
                     } catch (Exception e) {
                         closeEverything(socket, bufferedReader, bufferedWriter);
                     }
@@ -124,23 +141,39 @@ public class Cliente extends JFrame implements ActionListener{
         }).start();
     }
 
-    
-    public void actionPerformed(ActionEvent evento){
-                //System.out.println("Botao pressionado");
-                String mensagem = tfMensagem.getText();
-                sendMessage(mensagem);
-                tfMensagem.setText("");
-  
+    public void refreshList(String operation, String nameUser) {
+        this.modelList.clear();
+        this.modelList.addElement("All");
+        String[] names = nameUser.split(";");
+        for (String name : names) {
+            this.modelList.addElement(name);
+        }  
     }
-    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+
+
+    public void actionPerformed(ActionEvent evento) {
+        // System.out.println("Botao pressionado");
+        String mensagem = tfMensagem.getText();
+        sendMessage(mensagem);
+        tfMensagem.setText("");
+
+    }
+
+    public void valueChanged(ListSelectionEvent event){
+        if (event.getValueIsAdjusting()) {
+            this.whosWho =(String) list.getSelectedValue();
+        }        
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
         try {
-            if(bufferedReader != null){
+            if (bufferedReader != null) {
                 bufferedReader.close();
             }
-            if(bufferedWriter != null){
+            if (bufferedWriter != null) {
                 bufferedWriter.close();
             }
-            if(socket != null){
+            if (socket != null) {
                 socket.close();
             }
         } catch (Exception e) {
@@ -148,13 +181,13 @@ public class Cliente extends JFrame implements ActionListener{
         }
     }
 
-    //Main
-    public static void main(String args[]) throws IOException{
+    // Main
+    public static void main(String args[]) throws IOException {
         Scanner ler = new Scanner(System.in);
         System.out.print("Informe o nome de usuario: ");
         String username = ler.nextLine();
-        Socket socket = new Socket("localhost",12345);
-        Cliente cliente = new Cliente(socket,username);
+        Socket socket = new Socket("localhost", 12345);
+        Cliente cliente = new Cliente(socket, username);
         cliente.listenForMessage();
     }
 }
